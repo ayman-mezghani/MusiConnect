d6e6327f452534cab9995742b26bf2dd957b0a93
seboll13 <seboll13@gmail.com>
2020-02-28 11:50:49 +0100

    added main menu and corrected test coverage

--- app/src/main/java/ch/epfl/sdp/musiconnect/StartPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/StartPage.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import ch.epfl.sdp.R;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class StartPage extends AppCompatActivity {
 
@@ -11,5 +14,31 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.my_profile:
+                return true;
+                // In comments right now to avoid duplication
+            /*case R.id.map:
+                return true;
+            case R.id.search:
+                return true;
+            case R.id.settings:
+                return true;
+            case R.id.help:
+                return true;*/
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
