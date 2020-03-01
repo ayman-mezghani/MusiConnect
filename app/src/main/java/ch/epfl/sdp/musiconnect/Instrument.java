3a9e8aaed9427c097e07e4ff846f18463a832967
Manuel <manuel.pellegrini@epfl.ch>
2020-03-01 17:33:21 +0100

    Add instruments to Instrument enum

--- app/src/main/java/ch/epfl/sdp/musiconnect/Instrument.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/Instrument.java
@@ -1,4 +1,28 @@
 package ch.epfl.sdp.musiconnect;
 
 public enum Instrument {
+    ACCORDION,
+    BAGPIPES,
+    BANJO,
+    BASS,
+    CELLO,
+    CLARINET,
+    DRUMS,
+    FLUTE,
+    GUITAR,
+    HARP,
+    OBOE,
+    ORGAN,
+    PIANO,
+    SAXOPHONE,
+    TAMBOURINE,
+    TRIANGLE,
+    TROMBONE,
+    TRUMPET,
+    TUBA,
+    UKULELE,
+    VIOLA,
+    VIOLIN,
+    VOICE,
+    XYLOPHONE
 }
