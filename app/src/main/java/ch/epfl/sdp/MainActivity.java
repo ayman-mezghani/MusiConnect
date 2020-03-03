1709f9c63b8ff1e1b18c3ee4626ece1c18ef7b9e
Julien <hu.julien.ts4@gmail.com>
2020-03-03 14:32:59 +0100

    add (correct) gps implementation

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -2,11 +2,11 @@ package ch.epfl.sdp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import ch.epfl.sdp.musiconnect.UserLocationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.mainGoButton);
+        final Button button = findViewById(R.id.mainGoButton);
         button.setOnClickListener(v -> mainGoButton());
     }
 
     private void mainGoButton(){
-        Intent myIntent = new Intent(MainActivity.this, GreetingActivity.class);
+        Intent myIntent = new Intent(MainActivity.this, UserLocationActivity.class);
 
-        EditText e = findViewById(R.id.mainName);
-        myIntent.putExtra("msg", "Hello " + e.getText().toString() + "!"); //Optional parameters
         MainActivity.this.startActivity(myIntent);
     }
 
