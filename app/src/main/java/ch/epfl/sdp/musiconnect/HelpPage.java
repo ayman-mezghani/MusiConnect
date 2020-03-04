1ce1dde446520e3491bddc31f7825cd04f550826
seboll13 <seboll13@gmail.com>
2020-03-04 13:31:57 +0100

    update with new code coverage

--- app/src/main/java/ch/epfl/sdp/musiconnect/HelpPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/HelpPage.java
@@ -8,30 +8,20 @@ import android.view.MenuItem;
 
 public class HelpPage extends StartPage {
 
-    // The following methods contain helper code in order to avoid duplication !
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int x = 0;
-        if (x == 0)
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_help_page);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        int x = 0;
-        if (x+1 > 0)
-            return true;
-        return false;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
-        int x = 0;
-        if (x+1 > 0)
-            return super.onOptionsItemSelected(item);
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/ch/epfl/sdp/musiconnect/SettingsPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/SettingsPage.java
@@ -8,30 +8,20 @@ import android.view.MenuItem;
 
 public class SettingsPage extends StartPage {
 
-    // The following methods contain helper code in order to avoid duplication !
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int z = 0;
-        if (2*z == 0)
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_settings_page);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        int z = 0;
-        if (2*z == 0)
-            return true;
-        return false;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
-        int z = 0;
-        if (2*z == 0)
-            return super.onOptionsItemSelected(item);
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 }
