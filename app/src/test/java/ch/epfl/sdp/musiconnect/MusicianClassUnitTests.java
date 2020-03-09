06741507cd7c8010a37abcfb9efa7147ab62cc61
Manuel <manuel.pellegrini@epfl.ch>
2020-03-09 22:14:25 +0100

    Create test classes to test user profile classes

---
+++ app/src/test/java/ch/epfl/sdp/musiconnect/MusicianClassUnitTests.java
@@ -0,0 +1,13 @@
+package ch.epfl.sdp.musiconnect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Manuel Pellegrini, EPFL
+ */
+public class MusicianClassUnitTests {
+
+    // TODO
+
+}
