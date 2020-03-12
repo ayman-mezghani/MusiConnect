95e1bbbea5b582558d0293e092f35ba12ad67473
Manuel <manuel.pellegrini@epfl.ch>
2020-03-12 23:01:59 +0100

    Test

--- app/src/test/java/ch/epfl/sdp/musiconnect/MyDateClassUnitTests.java
+++ app/src/test/java/ch/epfl/sdp/musiconnect/MyDateClassUnitTests.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @author Manuel Pellegrini, EPFL
  */
 public class MyDateClassUnitTests {
+    
 
     @Test
     public void gettersOfMyDateClassWork() {
