29484f05ad29e858d7547adc056264a29ddc7cf2
ayman-mezghani <ayman.mezghani@epfl.ch>
2020-03-18 00:13:07 +0100

    Correct import and .travis.yml path to include covertage

--- app/src/test/java/ch/epfl/sdp/musiconnect/LocationClassUnitTests.java
+++ app/src/test/java/ch/epfl/sdp/musiconnect/LocationClassUnitTests.java
@@ -1,6 +1,6 @@
 package ch.epfl.sdp.musiconnect;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
