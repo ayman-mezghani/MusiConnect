0d2b33e47391727641b5a9d03dd725b9db3c95d4
Julien <hu.julien.ts4@gmail.com>
2020-03-19 20:02:14 +0100

    added test in location service

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/LocationServiceTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/LocationServiceTest.java
@@ -1,12 +1,23 @@
 package ch.epfl.sdp.musiconnect;
 
 
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SdkSuppress;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ServiceTestRule;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
@@ -16,6 +27,20 @@ public class LocationServiceTest {
     public final ServiceTestRule serviceRule =
             new ServiceTestRule();
 
+
+    @Test
+    public void testNullBinder() {
+        Intent serviceIntent =
+                new Intent(ApplicationProvider.getApplicationContext(), LocationService.class);
+        IBinder binder = null;
+        try {
+            binder = serviceRule.bindService(serviceIntent);
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(binder == null);
+    }
 
 
 }
