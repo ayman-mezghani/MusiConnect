21854c0ebf866053f85ed4e84413eaab8d288b8b
Julien <hu.julien.ts4@gmail.com>
2020-03-19 23:50:12 +0100

    optimize imports

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/MapsLocationTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/MapsLocationTest.java
@@ -1,10 +1,8 @@
 package ch.epfl.sdp.musiconnect;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
