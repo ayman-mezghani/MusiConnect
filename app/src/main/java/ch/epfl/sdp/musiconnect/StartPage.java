e07a93addffc310c9d4a96c201a0bda601d6cd1e
Julien <hu.julien.ts4@gmail.com>
2020-03-21 18:33:05 +0100

    Branch LocationStorage#59

--- app/src/main/java/ch/epfl/sdp/musiconnect/StartPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/StartPage.java
@@ -1,21 +1,73 @@
 package ch.epfl.sdp.musiconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import ch.epfl.sdp.R;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.location.Location;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 
 public class StartPage extends AppCompatActivity {
     private double lat = -34;
     private double lon = 151;
     private String mainMarkerName = "You";
+
+    private static final String TAG = "MainActivity";
+    private FusedLocationProviderClient fusedLocationClient;
+    private boolean locationPermissionGranted;
+    private Location userLocation;
+
+    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle b = intent.getBundleExtra("Location");
+            Location location = b.getParcelable("Location");
+            if (location != null) {
+                userLocation = location;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if(locationPermissionGranted){
+            getLastLocation();
+        } else {
+            checkLocationPermission();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                messageReceiver, new IntentFilter("GPSLocationUpdates"));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,5 +117,121 @@ public class StartPage extends AppCompatActivity {
         myIntent.putExtra("lon", lon);
         myIntent.putExtra("mainMarkerName", mainMarkerName);
         StartPage.this.startActivity(myIntent);
+    }
+
+
+    private void startLocationService() {
+        if (!isLocationServiceRunning()) {
+            Intent serviceIntent = new Intent(this, LocationService.class);
+
+            startService(serviceIntent);
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if("ch.epfl.sdp.musiconnect.LocationService".equals(service.service.getClassName())) {
+                Log.d(TAG, "isLocationServiceRunning: location service is already running.");
+                return true;
+            }
+        }
+        Log.d(TAG, "isLocationServiceRunning: location service is not running.");
+        return false;
+    }
+
+
+
+    private void getLastLocation() {
+        Log.d(TAG, "getLastKnownLocation: called.");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            checkLocationPermission();
+        } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                if (location != null) {
+                    // simply store value right now, may need to broadcast
+                    // or store in user information
+                    userLocation = location;
+
+                    // Start location updates
+                    startLocationService();
+                }
+            });
+        }
+    }
+
+
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", (dialogInterface, i) -> {
+                            //Prompt the user once explanation has been shown
+                            ActivityCompat.requestPermissions(this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    LocationService.MY_PERMISSIONS_REQUEST_LOCATION);
+                        })
+                        .setNegativeButton("cancel", (dialog, which) -> dialog.dismiss())
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LocationService.MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+
+        else {
+            locationPermissionGranted = true;
+            getLastLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LocationService.MY_PERMISSIONS_REQUEST_LOCATION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted. Do the location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, getString(R.string.perm_granted), Toast.LENGTH_LONG)
+                                .show();
+
+                        locationPermissionGranted = true;
+                        getLastLocation();
+                    }
+
+                } else {
+                    // permission denied. Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, getString(R.string.perm_denied), Toast.LENGTH_LONG)
+                            .show();
+
+                    locationPermissionGranted = false;
+                }
+
+                // other 'case' lines to check for other
+                // permissions this app might request
+
+        }
     }
 }
