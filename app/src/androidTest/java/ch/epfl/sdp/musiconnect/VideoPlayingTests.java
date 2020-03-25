e9574fec499eaec0340bf1b8cebb3468c5775412
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-03-25 02:04:44 +0100

    add video playing tests

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/VideoPlayingTests.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/VideoPlayingTests.java
@@ -14,7 +14,6 @@ import androidx.test.rule.ActivityTestRule;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -26,19 +25,53 @@ import ch.epfl.sdp.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
-public class VideoRecordingTests {
+public class VideoPlayingTests {
+    /**
+     * Function used to get an instance of an activity
+     * SOURCE : https://stackoverflow.com/questions/45829637/cannot-check-current-activity-from-drawerlayout-item
+     * @return the current activity running
+     */
+    public static Activity getCurrentActivity() {
+        try {
+            Class activityThreadClass = Class.forName("android.app.ActivityThread");
+            Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+            Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+            activitiesField.setAccessible(true);
+            Map activities = (Map) activitiesField.get(activityThread);
+            for (Object activityRecord : activities.values()) {
+                Class activityRecordClass = activityRecord.getClass();
+                Field pausedField = activityRecordClass.getDeclaredField("paused");
+                pausedField.setAccessible(true);
+                if (!pausedField.getBoolean(activityRecord)) {
+                    Field activityField = activityRecordClass.getDeclaredField("activity");
+                    activityField.setAccessible(true);
+                    Activity activity = (Activity) activityField.get(activityRecord);
+                    return activity;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @Rule
     public ActivityTestRule<StartPage> mActivityTestRule = new ActivityTestRule<>(StartPage.class);
@@ -53,14 +86,23 @@ public class VideoRecordingTests {
         appCompatTextView.perform(click());
     }
 
+    private String videoSource = "https://sites.google.com/site/androidexample9/download/RunningClock.mp4";
     @Test
-    public void videoRecordingTests() throws InterruptedException {
+    public void playVideoTest() throws InterruptedException {
         goToMyProfilePage();
 
-        ViewInteraction appCompatButton = onView(allOf(withText("Capture Video"),
-                        childAtPosition(childAtPosition(withClassName(is("android.widget.LinearLayout")),
-                                        0),1),isDisplayed()));
-        appCompatButton.perform(click());
+        ((ProfilePage) getCurrentActivity()).videoUri = Uri.parse(videoSource);
+
+        ViewInteraction videoView = onView(allOf(withId(R.id.videoView),
+                childAtPosition(childAtPosition(withClassName(is("android.widget.LinearLayout")),
+                        2),0),isDisplayed()));
+        //videoView.perform(click());
+
+        //onView(withId(R.id.videoView)).perform(click());
+
+        VideoView v = getCurrentActivity().findViewById(R.id.videoView);
+        //Thread.sleep(1000*15); // waiting util the vidéo is loaded from internet
+        //assertTrue(v.getDuration() > -1); // test if the video is loaded
     }
 
     private static Matcher<View> childAtPosition(
