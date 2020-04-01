e5620329b65969a077c01953c647677dd5317532
ayman-mezghani <ayman.mezghani@epfl.ch>
2020-04-01 16:08:57 +0200

    Create DB interface and testing mock

---
+++ app/src/main/java/ch/epfl/sdp/musiconnect/database/Database.java
@@ -0,0 +1,22 @@
+package ch.epfl.sdp.musiconnect.database;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Database {
+    static FirebaseFirestore getProdDB() {
+        return FirebaseFirestore.getInstance();
+    }
+
+    void addDoc(Map<String, Object> m, String docName);
+
+    void deleteDoc(String docName);
+
+    void updateDoc(String docName, Map<String, Object> newValueMap);
+
+    void deleteFieldsInDoc(String docName, List<String> fields);
+
+    void readDoc(String docName, DbCallback dbCallback);
+}
