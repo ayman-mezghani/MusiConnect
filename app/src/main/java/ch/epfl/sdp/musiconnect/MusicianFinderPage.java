7853e1a3e9d88955991fae0b585a21a8b966b242
Manuel <manuel.pellegrini@epfl.ch>
2020-04-02 18:04:17 +0200

    Implement the MusicianFinderPage class

--- app/src/main/java/ch/epfl/sdp/musiconnect/MusicianFinderPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/MusicianFinderPage.java
@@ -1,10 +1,56 @@
 package ch.epfl.sdp.musiconnect;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import ch.epfl.sdp.R;
+
 /**
  * @author Manuel Pellegrini, EPFL
  */
-public class MusicianFinderPage {
+public class MusicianFinderPage extends Page implements View.OnClickListener {
 
-    // TODO
+    private TextView title;
+    private TextView firstName;
+    private EditText musicianFirstName;
+    private TextView lastName;
+    private EditText musicianLastName;
+    private TextView userName;
+    private EditText musicianUserName;
+    private TextView instruments;
+    private EditText musicianInstruments;
+    private TextView levels;
+    private EditText musicianLevels;
+    private Button findMusician;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musician_finder_page);
+
+        title = findViewById(R.id.musicianFinderTitleID);
+        firstName = findViewById(R.id.musicianFinderFirstNameID);
+        musicianFirstName = findViewById(R.id.myMusicianFinderFirstNameID);
+        lastName = findViewById(R.id.musicianFinderLastNameID);
+        musicianLastName = findViewById(R.id.myMusicianFinderLastNameID);
+        userName = findViewById(R.id.musicianFinderUserNameID);
+        musicianUserName = findViewById(R.id.myMusicianFinderUserNameID);
+        instruments = findViewById(R.id.musicianFinderInstrumentsID);
+        musicianInstruments = findViewById(R.id.myMusicianFinderInstrumentsID);
+        levels = findViewById(R.id.musicianFinderLevelsID);
+        musicianLevels = findViewById(R.id.myMusicianFinderLevelsID);
+
+        // If the button findMusician is pressed, then the method onClick(view) is executed
+        findMusician = findViewById(R.id.musicianFinderButtonID);
+        findMusician.setOnClickListener(view -> onClick(view));
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.displayNotFinishedFunctionalityMessage();
+    }
 
 }
