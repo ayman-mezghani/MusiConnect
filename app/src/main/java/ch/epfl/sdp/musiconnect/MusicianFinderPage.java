41165e059ed4497d705a589a2297299039bb29af
Manuel <manuel.pellegrini@epfl.ch>
2020-04-02 20:04:30 +0200

    Implement the spinners in the musician finder

--- app/src/main/java/ch/epfl/sdp/musiconnect/MusicianFinderPage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/MusicianFinderPage.java
@@ -2,8 +2,10 @@ package ch.epfl.sdp.musiconnect;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Button;
 
 import ch.epfl.sdp.R;
@@ -21,9 +23,9 @@ public class MusicianFinderPage extends Page implements View.OnClickListener {
     private TextView userName;
     private EditText musicianUserName;
     private TextView instruments;
-    private EditText musicianInstruments;
+    private Spinner musicianInstruments;
     private TextView levels;
-    private EditText musicianLevels;
+    private Spinner musicianLevels;
     private Button findMusician;
 
     @Override
@@ -38,10 +40,24 @@ public class MusicianFinderPage extends Page implements View.OnClickListener {
         musicianLastName = findViewById(R.id.myMusicianFinderLastNameID);
         userName = findViewById(R.id.musicianFinderUserNameID);
         musicianUserName = findViewById(R.id.myMusicianFinderUserNameID);
+
         instruments = findViewById(R.id.musicianFinderInstrumentsID);
         musicianInstruments = findViewById(R.id.myMusicianFinderInstrumentsID);
+        // Create an ArrayAdapter using the string array instruments_array and a default spinner layout
+        ArrayAdapter<CharSequence> instrumentsAdapter = ArrayAdapter.createFromResource(this, R.array.instruments_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        instrumentsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the musicianInstruments spinner
+        musicianInstruments.setAdapter(instrumentsAdapter);
+
         levels = findViewById(R.id.musicianFinderLevelsID);
         musicianLevels = findViewById(R.id.myMusicianFinderLevelsID);
+        // Create an ArrayAdapter using the string array levels_array and a default spinner layout
+        ArrayAdapter<CharSequence> levelsAdapter = ArrayAdapter.createFromResource(this, R.array.levels_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        levelsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the musicianLevels spinner
+        musicianLevels.setAdapter(levelsAdapter);
 
         // If the button findMusician is pressed, then the method onClick(view) is executed
         findMusician = findViewById(R.id.musicianFinderButtonID);
