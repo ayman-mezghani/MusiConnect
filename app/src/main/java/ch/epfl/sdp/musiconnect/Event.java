5e9080ee96c82177441112477add5ac4f1a956c2
Julien <hu.julien.ts4@gmail.com>
2020-04-07 22:23:03 +0200

    create Event class

--- app/src/main/java/ch/epfl/sdp/musiconnect/Event.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/Event.java
@@ -2,32 +2,46 @@ package ch.epfl.sdp.musiconnect;
 
 import android.location.Location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
     private User creator;
+    private List<User> participants; // Users that will contribute to the event (e.g. two musicians doing a duo at x event)
     private Location location;
     private MyDate dateTime;
     private boolean visible;
+    private String title;
     private String message;
+    private final String defaultTitle = "Event";
     private final String defaultMessage = "Come watch and play!";
 
     public Event(User creator) {
         this.creator = creator;
+        participants = new ArrayList<>();
+        participants.add(creator);
+
         location = new Location("");
         dateTime = new MyDate();
         visible = false;
-        message = defaultMessage;
-    }
-
-    public Event(User creator, Location location, MyDate dateTime, boolean visible) {
-        this.creator  = creator;
-        this.location = location;
-        this.dateTime = dateTime;
-        this.visible  = visible;
+        title = defaultTitle;
         message = defaultMessage;
     }
 
     public User getCreator() {
         return creator;
+    }
+
+    public void register(User user) {
+        participants.add(user);
+    }
+
+    public void unregister(User user) {
+        participants.remove(user);
+    }
+
+    public List<User> getParticipants() {
+        return participants;
     }
 
     public void setLocation(Location location) {
@@ -52,6 +66,14 @@ public class Event {
 
     public boolean isVisible() {
         return visible;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setMessage(String message) {
