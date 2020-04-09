73e74adb54adb5f490474b0e44500e2d9a5bc456
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-04-09 15:33:27 +0200

    remove videoRecording button from my profile page

--- app/src/main/java/ch/epfl/sdp/musiconnect/MyProfilePage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/MyProfilePage.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -73,28 +72,15 @@ public class MyProfilePage extends ProfilePage implements View.OnClickListener {
         }
     }
 
-    public void captureVideo(View view) {
-        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-
-        if (videoIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(videoIntent, VIDEO_REQUEST);
-        }
-    }
-
     @Override
     public void onStart() {
         super.onStart();
-
         getVideoUri();
     }
 
     @SuppressLint("MissingSuperCall")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == VIDEO_REQUEST && resultCode == RESULT_OK) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-
         if (requestCode == LAUNCH_PROFILE_MODIF_INTENT && resultCode == Activity.RESULT_OK) {
             String[] newFields = data.getStringArrayExtra("newFields");
             assert newFields != null;
@@ -112,8 +98,6 @@ public class MyProfilePage extends ProfilePage implements View.OnClickListener {
             else{
                 getVideoUri();
             }
-
-
         }
 
 
