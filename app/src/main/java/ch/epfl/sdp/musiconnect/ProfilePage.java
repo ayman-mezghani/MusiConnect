05a01d97b460c2b8e3a1d02d915f0f798d8c36a1
ayman-mezghani <ayman.mezghani@epfl.ch>
2020-04-16 14:10:12 +0200

    Add default video for failing download and a test for coverage

--- app/src/main/java/ch/epfl/sdp/musiconnect/ProfilePage.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/ProfilePage.java
@@ -12,6 +12,8 @@ import com.google.firebase.storage.FirebaseStorage;
 
 import java.io.IOException;
 
+import ch.epfl.sdp.R;
+import ch.epfl.sdp.musiconnect.cloud.CloudCallback;
 import ch.epfl.sdp.musiconnect.cloud.CloudStorage;
 
 public abstract class ProfilePage extends Page {
@@ -49,9 +51,17 @@ public abstract class ProfilePage extends Page {
         String path = s + "/" + CloudStorage.FileType.video;
         String saveName = s + "_" + CloudStorage.FileType.video;
         try {
-            storage.download(path, saveName, fileUri -> {
-                videoUri = fileUri;
-                showVideo();
+            storage.download(path, saveName, new CloudCallback() {
+                @Override
+                public void onSuccess(Uri fileUri) {
+                    videoUri = fileUri;
+                    showVideo();
+                }
+                @Override
+                public void onFailure() {
+                    videoUri = Uri.parse("android.resource://"+getPackageName()+"/"+ R.raw.minion);
+                    showVideo();
+                }
             });
         } catch (IOException e) {
             Toast.makeText(this, "An error occured, please contact support.", Toast.LENGTH_LONG).show();
