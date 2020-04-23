3bb55bd258c0a01e55e1724ade1125fea0f3a114
TigreBois <matthieu.masouye@epfl.ch>
2020-04-23 19:07:53 +0200

    Streamlined checkconnection

--- app/src/main/java/ch/epfl/sdp/musiconnect/ConnectionCheck.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/ConnectionCheck.java
@@ -9,12 +9,8 @@ public class ConnectionCheck {
 
     public static boolean checkConnection(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            //we are connected to a network
-            return true;
-        } else {
-            return false;
-        }
+        return (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED);
     }
+
 }
