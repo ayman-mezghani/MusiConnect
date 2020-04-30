b4d3aef82112bc27251bfe2c6041a8229e4dffec
Manuel <manuel.pellegrini@epfl.ch>
2020-04-30 17:01:44 +0200

    Write tests for MusicianFinderPage

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/MusicianFinderPageTests.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/MusicianFinderPageTests.java
@@ -1,8 +1,80 @@
 package ch.epfl.sdp.musiconnect;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sdp.R;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * @author Manuel Pellegrini, EPFL
  */
+@RunWith(AndroidJUnit4.class)
 public class MusicianFinderPageTests {
-    // TODO
+
+    private String firstName = "John";
+    private String lastName = "Lennon";
+    private String userName = "JohnLennon";
+
+    @Rule
+    public final ActivityTestRule<MusicianFinderPage> musicianFinderPageRule = new ActivityTestRule<>(MusicianFinderPage.class);
+
+    private UiDevice device;
+
+    // Before and after methods are used in order to accept tests with intents
+    @Before
+    public void initIntents() {
+        Intents.init();
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        MapsLocationTest.clickAlert(device);
+    }
+
+    @After
+    public void releaseIntents() { Intents.release(); }
+
+    @Test
+    public void testEditTextFieldsOfMusicianFinderWork() {
+        onView(withId(R.id.myMusicianFinderFirstNameID)).perform(ViewActions.scrollTo()).perform(clearText(), typeText(firstName));
+        onView(withId(R.id.myMusicianFinderLastNameID)).perform(ViewActions.scrollTo()).perform(clearText(), typeText(lastName));
+        onView(withId(R.id.myMusicianFinderUserNameID)).perform(ViewActions.scrollTo()).perform(clearText(), typeText(userName));
+        closeSoftKeyboard();
+
+        onView(withId(R.id.myMusicianFinderFirstNameID)).check(matches(withText(firstName)));
+        onView(withId(R.id.myMusicianFinderLastNameID)).check(matches(withText(lastName)));
+        onView(withId(R.id.myMusicianFinderUserNameID)).check(matches(withText(userName)));
+    }
+
+    @Test
+    public void testSpinnerFieldsOfMusicianFinderWork() {
+        // TODO
+        assert(true);
+    }
+
+    @Test
+    public void testFindMusicianButtonWorks() {
+        onView(withId(R.id.musicianFinderButtonID)).perform(scrollTo(), click());
+
+        // This button does nothing yet
+        assert(true);
+    }
+
 }
