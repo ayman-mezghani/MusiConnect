04badc665986a8946f0c3d3e1bea18a234a8b791
Julien <hu.julien.ts4@gmail.com>
2020-05-03 19:17:09 +0200

    fix code from reviews

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/MapsLocationTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/MapsLocationTest.java
@@ -1,9 +1,5 @@
 package ch.epfl.sdp.musiconnect;
 
-import android.os.Looper;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SdkSuppress;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
@@ -11,18 +7,9 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import org.junit.BeforeClass;
 import org.junit.Rule;
-import org.junit.runner.RunWith;
-
-import ch.epfl.sdp.musiconnect.cloud.CloudStorageGenerator;
-import ch.epfl.sdp.musiconnect.cloud.MockCloudStorage;
-import ch.epfl.sdp.musiconnect.database.DbGenerator;
-import ch.epfl.sdp.musiconnect.database.MockDatabase;
 
 
-@RunWith(AndroidJUnit4.class)
-@SdkSuppress(minSdkVersion = 18)
 public class MapsLocationTest {
 
 
@@ -31,24 +18,12 @@ public class MapsLocationTest {
             new ActivityTestRule<>(MapsActivity.class);
 
 
-
-    @BeforeClass
-    public static void set() {
-        Looper.prepare();
-    }
-
-    @BeforeClass
-    public static void setMocks() {
-        DbGenerator.setDatabase(new MockDatabase());
-        CloudStorageGenerator.setStorage((new MockCloudStorage()));
-    }
-
     /**
      * Code to click on the alerts has been found here:
      * https://gist.github.com/rocboronat/65b1187a9fca9eabfebb5121d818a3c4
      */
 
-    public static void clickAlert(UiDevice device) {
+    static void clickAlert(UiDevice device) {
         try {
             UiObject alert = device.findObject(new UiSelector().className("android.widget.Button")
                     .text("OK"));
@@ -61,7 +36,7 @@ public class MapsLocationTest {
         }
     }
 
-    public static void clickOnDialog(UiDevice device, int pos) {
+    private static void clickOnDialog(UiDevice device, int pos) {
         try {
             UiObject allowPermissions = device.findObject(new UiSelector()
                     .clickable(true)
@@ -81,7 +56,7 @@ public class MapsLocationTest {
     /**
      * Clicks on the alert boxes such that location permissions are given
      */
-    public static void clickAllow() {
+    static void clickAllow() {
         UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         clickAlert(device);
         // clickOnDialog(device, 1);
@@ -90,9 +65,11 @@ public class MapsLocationTest {
     /**
      * Clicks on the alert boxes such that location permissions are rejected
      */
+    /*
     public static void clickDeny() {
         UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         clickAlert(device);
         clickOnDialog(device, 0);
     }
+    */
 }
