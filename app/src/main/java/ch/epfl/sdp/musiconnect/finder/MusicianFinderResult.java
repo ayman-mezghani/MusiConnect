1146822da70f876940ebaca7a8954a04f64cccea
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-05-06 17:35:55 +0200

    finder musician done

--- app/src/main/java/ch/epfl/sdp/musiconnect/finder/MusicianFinderResult.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/finder/MusicianFinderResult.java
@@ -1,11 +1,11 @@
 package ch.epfl.sdp.musiconnect.finder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 
 import ch.epfl.sdp.R;
-import ch.epfl.sdp.musiconnect.StartPage;
 import ch.epfl.sdp.musiconnect.User;
 import ch.epfl.sdp.musiconnect.database.DbCallback;
 import ch.epfl.sdp.musiconnect.database.DbGenerator;
@@ -37,6 +36,26 @@ public class MusicianFinderResult extends AppCompatActivity {
         Intent intent = getIntent();
         Map<String, Object> searchMap = (HashMap<String, Object>)intent.getSerializableExtra("searchMap");
 
+        for(Map.Entry<String, Object> entry : searchMap.entrySet()) {
+            String key = entry.getKey();
+            String value = (String) entry.getValue();
+
+            HashMap<String, Object> innerMap = new HashMap<>();
+            innerMap.put(key, value);
+
+            DbGenerator.getDbInstance().query(DbUserType.Musician, innerMap, new DbCallback() {
+                @Override
+                public void queryCallback(List<User> userList) {
+                for (User u: userList) {
+                    if(!listMusician.contains((String) u.getEmailAddress()))
+                        listMusician.add(u.getEmailAddress());
+                }
+                adapter.notifyDataSetChanged();
+                }
+            });
+        }
+
+        /*
         DbGenerator.getDbInstance().query(DbUserType.Musician, searchMap, new DbCallback() {
             @Override
             public void queryCallback(List<User> userList) {
@@ -46,5 +65,6 @@ public class MusicianFinderResult extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+        */
     }
 }
