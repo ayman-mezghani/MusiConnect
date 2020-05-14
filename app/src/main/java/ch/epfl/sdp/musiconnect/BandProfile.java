f3ec815b18904fc0d3331378a4075e57ff0cfe70
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-05-14 15:52:04 +0200

    add click on band finer result¨

--- app/src/main/java/ch/epfl/sdp/musiconnect/BandProfile.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/BandProfile.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,52 +18,48 @@ import ch.epfl.sdp.musiconnect.database.DbGenerator;
 import ch.epfl.sdp.musiconnect.database.DbUserType;
 
 public class BandProfile extends Page {
-
+// LeaderMail
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_band_profile);
 
-        Spinner bandSpinner = findViewById(R.id.spinnerBands);
+        if(getIntent().hasExtra("leaderMail")) {
+            loadProfileContent();
+        } else {
+            Spinner bandSpinner = findViewById(R.id.spinnerBands);
 
-        Button bandEditProfile = findViewById(R.id.btnEditBandProfile);
-        if(CurrentUser.getInstance(this).getTypeOfUser() == TypeOfUser.Band) {
-            bandEditProfile.setVisibility(View.VISIBLE);
-            bandEditProfile.setFocusable(true);
-            bandEditProfile.setClickable(true);
-        }
+            List<String> spinnerArray = new ArrayList<String>();
 
-        // you need to have a list of data that you want the spinner to display
-        List<String> spinnerArray = new ArrayList<String>();
-
-        for (Band b : CurrentUser.getInstance(this).getBands()) {
-            spinnerArray.add(b.getName());
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, spinnerArray);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        bandSpinner.setAdapter(adapter);
-
-        bandSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                String selected = bandSpinner.getSelectedItem().toString();
-                Toast.makeText(BandProfile.this, selected, Toast.LENGTH_SHORT).show();
-
-                DbGenerator.getDbInstance().read(DbUserType.Band,
-                        getBandFromName(CurrentUser.getInstance(BandProfile.this).getBands(), selected).getEmailAddress(), new DbCallback() {
-                            @Override
-                            public void readCallback(User user) {
-                                displayBandInfo((Band) user);
-                            }
-                        });
+            for (Band b : CurrentUser.getInstance(this).getBands()) {
+                spinnerArray.add(b.getName());
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {}
-        });
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_spinner_item, spinnerArray);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            bandSpinner.setAdapter(adapter);
+
+            bandSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                    String selected = bandSpinner.getSelectedItem().toString();
+                    Toast.makeText(BandProfile.this, selected, Toast.LENGTH_SHORT).show();
+
+                    DbGenerator.getDbInstance().read(DbUserType.Band,
+                            getBandFromName(CurrentUser.getInstance(BandProfile.this).getBands(), selected).getEmailAddress(), new DbCallback() {
+                                @Override
+                                public void readCallback(User user) {
+                                    displayBandInfo((Band) user);
+                                }
+                            });
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> arg0) {}
+            });
+        }
     }
 
     private void displayBandInfo(Band b) {
@@ -86,11 +81,30 @@ public class BandProfile extends Page {
         lvBandEvent.setAdapter(adapter2);
     }
 
-    private Band getBandFromName(List<Band> lb, String bandName) {
+    public static Band getBandFromName(List<Band> lb, String bandName) {
         for(Band b: lb) {
             if(b.getName().equals(bandName))
                 return b;
         }
         return null;
+    }
+
+    private void loadProfileContent() {
+        String leaderEmail = getIntent().getStringExtra("leaderMail");
+        DbGenerator.getDbInstance().read(DbUserType.Band, leaderEmail, new DbCallback() {
+            @Override
+            public void readCallback(User user) {
+                displayBandInfo((Band) user);
+            }
+
+            @Override
+            public void readFailCallback() {
+                    loadNullProfile();
+                }
+            });
+    }
+
+    private void loadNullProfile() {
+        setContentView(R.layout.activity_visitor_profile_page_null);
     }
 }
--- app/src/main/java/ch/epfl/sdp/musiconnect/finder/BandFinderResult.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/finder/BandFinderResult.java
@@ -1,11 +1,11 @@
 package ch.epfl.sdp.musiconnect.finder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,12 +13,15 @@ import java.util.List;
 import java.util.Map;
 
 import ch.epfl.sdp.R;
+import ch.epfl.sdp.musiconnect.Band;
+import ch.epfl.sdp.musiconnect.BandProfile;
 import ch.epfl.sdp.musiconnect.User;
 import ch.epfl.sdp.musiconnect.database.DbCallback;
 import ch.epfl.sdp.musiconnect.database.DbGenerator;
 import ch.epfl.sdp.musiconnect.database.DbUserType;
 
 public class BandFinderResult extends AppCompatActivity {
+    List<Band> listBand = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +29,15 @@ public class BandFinderResult extends AppCompatActivity {
         setContentView(R.layout.activity_band_finder_result);
 
 
-        List<String> listMusician = new ArrayList<>();
+        List<String> listBandShow = new ArrayList<>();
 
         ListView lvMusicianResult = findViewById(R.id.LvBandResult);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<>
-                (BandFinderResult.this, android.R.layout.simple_list_item_1, listMusician);
+                (BandFinderResult.this, android.R.layout.simple_list_item_1, listBandShow);
         lvMusicianResult.setAdapter(adapter);
+
+        listViewAddOnItemClickListner(lvMusicianResult);
 
         Intent intent = getIntent();
         Map<String, Object> searchMap = (HashMap<String, Object>)intent.getSerializableExtra("searchMap");
@@ -48,24 +53,22 @@ public class BandFinderResult extends AppCompatActivity {
                 @Override
                 public void queryCallback(List<User> userList) {
                     for (User u: userList) {
-                        if(!listMusician.contains((String) u.getEmailAddress()))
-                            listMusician.add(u.getEmailAddress());
+                        if(!listBandShow.contains((String) u.getEmailAddress())) {
+                            listBandShow.add(u.getName());
+                            listBand.add((Band) u);
+                        }
                     }
                     adapter.notifyDataSetChanged();
                 }
             });
         }
+    }
 
-        /*
-        DbGenerator.getDbInstance().query(DbUserType.Musician, searchMap, new DbCallback() {
-            @Override
-            public void queryCallback(List<User> userList) {
-                for (User u: userList) {
-                    listMusician.add(u.getEmailAddress());
-                }
-                adapter.notifyDataSetChanged();
-            }
+    private void listViewAddOnItemClickListner(ListView l) {
+        l.setOnItemClickListener((parent, view, position, id) -> {
+            Intent i = new Intent(BandFinderResult.this, BandProfile.class);
+            i.putExtra("leaderMail", BandProfile.getBandFromName(listBand, (String) l.getItemAtPosition(position)).getEmailAddress());
+            startActivity(i);
         });
-        */
     }
 }
