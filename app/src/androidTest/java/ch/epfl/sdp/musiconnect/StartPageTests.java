4f1cd3f0982e84713d2082b3aabd27d7b84e0251
Antonio Pisanello <antonio.pisanello@epfl.ch>
2020-05-14 14:00:53 +0200

    fix my band menu clicking

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/StartPageTests.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/StartPageTests.java
@@ -1,7 +1,9 @@
 package ch.epfl.sdp.musiconnect;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,13 +15,16 @@ import ch.epfl.sdp.musiconnect.database.DbGenerator;
 import ch.epfl.sdp.musiconnect.database.MockDatabase;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static ch.epfl.sdp.musiconnect.testsFunctions.childAtPosition;
 import static ch.epfl.sdp.musiconnect.testsFunctions.getCurrentActivity;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertTrue;
 
 public class StartPageTests {
@@ -35,6 +40,13 @@ public class StartPageTests {
         CloudStorageGenerator.setStorage((new MockCloudStorage()));
     }
 
+    @Before
+    public void setCurrentUser() {
+        CurrentUser.getInstance(activityRule.getActivity()).email = "musiconnectsdp@gmail.com";
+        CurrentUser.getInstance(activityRule.getActivity()).setTypeOfUser(TypeOfUser.Band);
+        CurrentUser.getInstance(activityRule.getActivity()).setBand(md.getBand());
+    }
+
     @Test
     public void updateUserGetBand() {
         Musician musiConnect = md.getDummyMusician(4);
@@ -43,13 +55,32 @@ public class StartPageTests {
         assertTrue(CurrentUser.getInstance(activityRule.getActivity()).getBand().getName().equals(md.getBand().getName()));
     }
 
-    //@Test
+    @Test
     public void testBandProfileClickShouldStartNewIntent() {
-        CurrentUser.getInstance(activityRule.getActivity()).email = "musiconnectsdp@gmail.com";
-        CurrentUser.getInstance(activityRule.getActivity()).setTypeOfUser(TypeOfUser.Band);
-        CurrentUser.getInstance(activityRule.getActivity()).setBand(md.getBand());
+        /*
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        onView(withText(R.string.my_bands)).perform(click());
+        onView(withId(R.id.my_profileBand)).perform(scrollTo(), click());
+        */
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                2),
+                        isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("My bands"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
         intended(hasComponent(BandProfile.class.getName()));
     }
 }
