f3699ddc359b1542cb9c79b6c9b7c10b642b220a
seboll13 <seboll13@gmail.com>
2020-05-30 15:19:35 +0200

    added one more test

--- app/src/androidTest/java/ch/epfl/sdp/musiconnect/DarkModeTests.java
+++ app/src/androidTest/java/ch/epfl/sdp/musiconnect/DarkModeTests.java
@@ -3,9 +3,13 @@ package ch.epfl.sdp.musiconnect;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Checkable;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,15 +18,21 @@ import org.junit.runner.RunWith;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import ch.epfl.sdp.R;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static ch.epfl.sdp.musiconnect.StartPage.KEY_ISNIGHTMODE;
 import static ch.epfl.sdp.musiconnect.StartPage.MY_PREFERENCES;
 import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
@@ -67,5 +77,44 @@ public class DarkModeTests {
         sp.edit().putBoolean(KEY_ISNIGHTMODE, false).apply();
 
         assertFalse(sp.getBoolean(KEY_ISNIGHTMODE, false));
+    }
+
+    public static ViewAction setChecked(final boolean checked) {
+        return new ViewAction() {
+            @Override
+            public BaseMatcher<View> getConstraints() {
+                return new BaseMatcher<View>() {
+                    @Override
+                    public boolean matches(Object item) {
+                        return isA(Checkable.class).matches(item);
+                    }
+
+                    @Override
+                    public void describeMismatch(Object item, Description mismatchDescription) {}
+
+                    @Override
+                    public void describeTo(Description description) {}
+                };
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                Checkable checkableView = (Checkable) view;
+                checkableView.setChecked(checked);
+            }
+        };
+    }
+
+    @Test
+    public void testNightModeStateChangeDoesChange() {
+        onView(withId(R.id.darkModeSwitch)).perform(scrollTo(), setChecked(true));
+
+        TextView title = startPageRule.getActivity().findViewById(R.id.welcomeText);
+        assertThat(title.getCurrentTextColor(), is(ContextCompat.getColor(startPageRule.getActivity().getApplicationContext(), R.color.text_grey)));
     }
 }
