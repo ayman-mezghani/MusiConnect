203bf278fb293fa4b05eacc344822693f4d12584
TigreBois <matthieu.masouye@epfl.ch>
2020-06-02 21:33:07 +0200

    Testing

--- app/src/main/java/ch/epfl/sdp/musiconnect/location/MapsActivity.java
+++ app/src/main/java/ch/epfl/sdp/musiconnect/location/MapsActivity.java
@@ -200,7 +200,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 spinner.setSelection(position);
 
-                circle.setRadius(threshold);
+                
 
                 updateProfileList();
                 loadProfilesMarker();
